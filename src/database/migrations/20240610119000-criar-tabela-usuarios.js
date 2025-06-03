3d6850d2fce884c118cec7508679277801d4aba9
AdemirRed <ademir1de1oliveira@gmail.com>
2025-06-03 19:00:03 -0300

    Corrige formatação do código na migração da tabela de usuários, mantendo a consistência na definição da chave primária.

--- src/database/migrations/20240610119000-criar-tabela-usuarios.js
+++ src/database/migrations/20240610119000-criar-tabela-usuarios.js
@@ -6,7 +6,7 @@ module.exports = {
       id: {
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4,
-        primaryKey: true,
+        primaryKey: true, 
         allowNull: false,
       },
       nome: {
